4249359a8ef1a00d0808dc1c67988cce30cd2aed
Ryan Trad <trad.ryan.alan@gmail.com>
2018-04-04 14:38:24 -0400

    added text search to index and formatted food_item

--- server.py
+++ server.py
@@ -1,10 +1,11 @@
 from flask import Flask, request, render_template, json
+from bson import json_util, objectid
 from pymongo import MongoClient
 
 
 
 dbclient = MongoClient('localhost', 27017)
-db = db.client.foodoptionsdb
+db = dbclient.foodoptionsdb
 
 app = Flask(__name__)
 
@@ -13,6 +14,19 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/fooditem/<id>')
+def food_item(id):
+    item = db.foods.find_one({'_id' : objectid.ObjectId(id)})
+    return render_template('food_item.html', item=item)
+    
+@app.route('/search', methods=['POST'])
+def search():
+    text = request.form['textvalue'].strip()
+    if len(text) > 0:
+        items = db.foods.find({'$text' : {'$search' : request.form['textvalue']}})
+    else:
+        items = db.foods.find({})
+    return json_util.dumps(items)
 
 if __name__ == '__main__':
     app.run(debug=True)
